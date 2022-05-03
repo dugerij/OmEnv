8f5d8941c89c720c0e271cbbd6828ceea544bef4
Dugeri James <dugeri.james@gmail.com>
2022-05-03 23:20:36 +0100

    new

--- APi_practice/app.py
+++ APi_practice/app.py
@@ -5,21 +5,39 @@ class ObjRequestClass:
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         data = json.loads(req.stream.read())
-        
-        content = {
-            'name': 'Paris',
-            'age': '31',
-            'country': 'Denmark'
+        output = {
+            'msg': 'Hello {0}'.format(data['name'])
         }
-        output = {}
-        if (data['method'] == 'get-name'):
-            output['value'] = content['name']
-        else:
-            resp.status = falcon.HTTP_404
-            output['value'] = None
+      
         resp.body = json.dumps(output)
        
+    def on_post(self, req, resp):
+        resp.status = falcon.HTTP_200
+        data = json.loads(req.stream.read())
+
+        equel = int(data['x']) + int(data['y'])
+        output = {
+            'msg': 'x: {0} + y: {1} is equal to {2}'.format(str(data['x']), str(data['y']), str(equel))
+        }
+      
+        resp.body = json.dumps(output)
+    
+    def on_put(self, req, resp):
+        resp.status = falcon.HTTP_200
+        output = {
+            'msg': 'put is not supported for now - sorry :('
+        }
+
+        resp.body = json.dumps(output)
+
+    def on_delete(self, req, resp):
+        resp.status = falcon.HTTP_200
+        output = {
+            'msg': 'delete is not supported for now - sorry :('
+        }
+
+        resp.body = json.dumps(output)
 
 app = application = falcon.App()
 repo = ObjRequestClass()
-app.add_route('/test', repo ) 
+app.add_route('/demo', repo ) 
