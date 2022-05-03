148de3b2eec57db15bbc7f46e6a412d140bfe0f3
Dugeri James <dugeri.james@gmail.com>
2022-05-04 00:17:07 +0100

    ne

--- APi_practice/app.py
+++ APi_practice/app.py
@@ -2,13 +2,36 @@ from asyncio import DatagramTransport
 import json, falcon
 
 class ObjRequestClass:
+    __json_content = {}
+    def __validate_json_input(self, req):
+        try:
+            self.__json_content = json.loads(req.stream.read())
+            print('json from client is validated')
+            return True
+
+        except ValueError, e:
+            self.__json_content = {}
+            print('Json from client is not validated')
+            return False
+
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
-        data = json.loads(req.stream.read())
+        validated =  self.__validate_json_input(req)
+
         output = {
-            'msg': 'Hello {0}'.format(data['name'])
+            'status': 200,
+            'msg': None
         }
-      
+
+        if (validated==True):
+
+            output['msg'] = 'Hello {name}'.format(name=self.__json_content['name'])
+    
+        else:
+            output['status'] = 404
+            output['msg'] = 'Json input is not validated'
+
+
         resp.body = json.dumps(output)
        
     def on_post(self, req, resp):
