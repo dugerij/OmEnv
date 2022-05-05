a4ea0fa9b650fc5b60d963a40d318b50ad07dd84
Dugeri James <dugeri.james@gmail.com>
2022-05-05 20:58:02 +0100

    new

--- APi/API/predict.py
+++ APi/API/predict.py
@@ -7,24 +7,11 @@ import pandas as pd
 import random
 import scorecardpy as sc
 
-import torch
-import skorch
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-import xgboost as xgb
-# import lightgbm
-# from lightgbm import LGBMClassifier
-
 random.seed(3)
 
-
-X_test = {'customerId': 202109203271, 'personalDetails.typeOfID': 'PVC', 'personalDetails.gender': 'male', 'personalDetails.religion': 'Islam', 'personalDetails.maritalStatus': 'Married', 'personalDetails.numberOfChildren': 2.0, 'business.dailyIncome': '11000', 'business.type': 'Transport', 'business.weeklyMarketAttendance': 6.0, 'business.yearsInBusiness': 6.0, 'guarantor.typeOfID': 'PVC', 'guarantor.gender': 'male', 'guarantor.religion': 'Islam', 'guarantor.business.monthlyIncome': '50000-200000', 'guarantor.business.type': 'AUTOMOBILE MECHANIC', 'guarantor.business.yearsInBusiness': 6.0, 'Dibursement Date': '2021-10-06T16:20:07.611Z', 'Disbursed Amount': 210000.0, 'Repayment Start Date': '2021-10-11T16:20:06.178Z', 'Repayment End Date': '2021-11-02T16:20:06.178Z', 'Loan Balance': 0.0}
-
 def process_event(raw_json):
-    # input_dict = json.loads(raw_json.decode())
-    # raw_json= {'customerId': 202109203271, 'personalDetails.typeOfID': 'PVC', 'personalDetails.gender': 'male', 'personalDetails.religion': 'Islam', 'personalDetails.maritalStatus': 'Married', 'personalDetails.numberOfChildren': 2.0, 'business.dailyIncome': '11000', 'business.type': 'Transport', 'business.weeklyMarketAttendance': 6.0, 'business.yearsInBusiness': 6.0, 'guarantor.typeOfID': 'PVC', 'guarantor.gender': 'male', 'guarantor.religion': 'Islam', 'guarantor.business.monthlyIncome': '50000-200000', 'guarantor.business.type': 'AUTOMOBILE MECHANIC', 'guarantor.business.yearsInBusiness': 6.0, 'Dibursement Date': '2021-10-06T16:20:07.611Z', 'Disbursed Amount': 210000.0, 'Repayment Start Date': '2021-10-11T16:20:06.178Z', 'Repayment End Date': '2021-11-02T16:20:06.178Z', 'Loan Balance': 0.0}
-    X_test = raw_json
-    X_test = pd.DataFrame(X_test, index=[0], columns=list(X_test.keys()))
+    input_dict = raw_json
+    X_test = pd.DataFrame(input_dict, index=[0], columns=list(input_dict.keys()))
     
     def date_columns_conv(df, column):
         today_date = (pd.to_datetime('2022-04-15', utc=True))
@@ -90,39 +77,59 @@ def process_event(raw_json):
 
     return X_test
 
+def process_card_data(raw_json):
+    input_dict = raw_json
+    X_test = pd.DataFrame(input_dict, index=[0], columns=list(input_dict.keys()))
+    
+    def date_columns_conv(df, column):
+        today_date = (pd.to_datetime('2022-04-15', utc=True))
+ 
+        df[column] = (pd.to_datetime(df[column], format='%Y-%m-%d'))
+        df['mths_since_' + column] = round(pd.to_numeric((today_date - df[column]) / np.timedelta64(1, 'M')))
+    
+        df['mths_since_' + column] = df['mths_since_' + column].apply(lambda x: df['mths_since_' + column].max() if x<0 else x)
+        df.drop(columns=[column], inplace=True)
 
-# input_dict = json.loads(raw_json.decode())       
-X = X_test.copy()
-data = process_event(X)
+    date_columns_conv(X_test, 'Dibursement Date')
+
+    # We will drop future looking columns i.e  'Repayment Start Date', 'Repayment End Date' as well as 'Loan Balance' as it will have similar implications as 'Days Outstanding' 
+    X_test.drop(columns=['Repayment Start Date', 'Repayment End Date', 'Loan Balance'], inplace=True)
+    
+    # Change daily income from object to numbers
+    X_test['business.dailyIncome'] = pd.to_numeric(X_test['business.dailyIncome'])
+
+    # 
+    X_test.drop(columns=['customerId'], inplace=True)
+
+    # Drop unhelpful columns gender and religion and business.type
+    X_test.drop(columns=['personalDetails.gender', 'personalDetails.religion', 'business.type', 'guarantor.gender', 'guarantor.religion', 'guarantor.business.type'], inplace=True)
+
+    return X_test
 
 class PredictResource(object):
-
     def __init__(self, model):
         self.model = model
 
-    def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
-        resp.body = 'Hello World!'
-
     def on_post(self, req, resp):
-        predicted_data = self.model.predict(data)
+        data = req.get_media()
+        data = process_event(raw_json=data)
 
+        predicted_data = self.model.predict(data)
         output = {'prediction': str(predicted_data)}
-        resp.status = falcon.HTTP_201
+
+        resp.status = falcon.HTTP_200
         resp.body = json.dumps(output, ensure_ascii=False)
 
 class GenerateCreditScore(object):
     def __init__(self, card):
         self.card = card
 
-    def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
-        resp.body = 'Hi'
-
     def on_post(self, req, resp):
-        score = sc.scorecard_ply(X_test, self.card, only_total_score=True, print_step=0, replace_blank_na=True, var_kp = None)
-        output = {'Credit score': str(score)}
+        data = req.get_media()
+        data = process_card_data(data)
 
-        resp.status = falcon.HTTP_201
+        score = sc.scorecard_ply(data, self.card, only_total_score=True, print_step=0, replace_blank_na=True, var_kp = None)
+        output = {'Credit score': (str(score))[-5:]}
+
+        resp.status = falcon.HTTP_200
         resp.body = json.dumps(output, ensure_ascii=False)
-        resp.location = '/creditscore/score'
--- APi_practice/app.py
+++ APi_practice/app.py
@@ -1,69 +1,100 @@
-from asyncio import DatagramTransport
-import json, falcon
+import falcon
 
-class ObjRequestClass:
-    __json_content = {}
-    def __validate_json_input(self, req):
-        try:
-            self.__json_content = json.loads(req.stream.read())
-            print('json from client is validated')
-            return True
+class Authorize(object):
+    def __init__(self, roles):
+        self._roles = roles
 
-        except ValueError:
-            self.__json_content = {}
-            print('Json from client is not validated')
-            return False
-
-    def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
-        validated =  self.__validate_json_input(req)
-
-        output = {
-            'status': 200,
-            'msg': None
-        }
-
-        if (validated==True):
-            if 'name' in self.__json_content:
-                output['msg'] = 'Hello {name}'.format(name=self.__json_content ['name'])
-            else:
-                output['status'] = 404
-                output['msg'] = 'Json input need name'
+    def __call__(self, req, resp, resource, params):
+        if 'Admin' in self._roles:
+            req.user_id = 5
 
         else:
-            output['status'] =  404
-            output['msg'] = 'Json input is not validated'
+            raise falcon.HTTPBadRequest('Bad REquest', 'YOu are not an admin right now')
+        
 
+class ObjRequestClass:
+    @falcon.before(Authorize(['Admin', 'Normal', 'Guest']))
+    def on_get(self, req, resp):
+        print('trigger - get')
 
-        resp.body = json.dumps(output)
-       
+        output = {
+            'method': 'get',
+            'user_id': req.user_id
+        }
+
+        resp.media = output
+
     def on_post(self, req, resp):
-        resp.status = falcon.HTTP_200
-        data = json.loads(req.stream.read())
+        print('trigger - post')
 
-        equel = int(data['x']) + int(data['y'])
         output = {
-            'msg': 'x: {0} + y: {1} is equal to {2}'.format(str(data['x']), str(data['y']), str(equel))
+            'method': 'post',
+            'user_id': req.user_id
         }
+        resp.media = output
+
+    # __json_content = {}
+    # def __validate_json_input(self, req):
+    #     try:
+    #         self.__json_content = json.loads(req.stream.read())
+    #         print('json from client is validated')
+    #         return True
+
+    #     except ValueError:
+    #         self.__json_content = {}
+    #         print('Json from client is not validated')
+    #         return False
+
+    # def on_get(self, req, resp):
+    #     resp.status = falcon.HTTP_200
+    #     validated =  self.__validate_json_input(req)
+
+    #     output = {
+    #         'status': 200,
+    #         'msg': None
+    #     }
+
+    #     if (validated==True):
+    #         if 'name' in self.__json_content:
+    #             output['msg'] = 'Hello {name}'.format(name=self.__json_content ['name'])
+    #         else:
+    #             output['status'] = 404
+    #             output['msg'] = 'Json input need name'
+
+    #     else:
+    #         output['status'] =  404
+    #         output['msg'] = 'Json input is not validated'
+
+
+    #     resp.body = json.dumps(output)
+       
+    # def on_post(self, req, resp):
+    #     resp.status = falcon.HTTP_200
+    #     data = json.loads(req.stream.read())
+
+    #     equel = int(data['x']) + int(data['y'])
+    #     output = {
+    #         'msg': 'x: {0} + y: {1} is equal to {2}'.format(str(data['x']), str(data['y']), str(equel))
+    #     }
       
-        resp.body = json.dumps(output)
+    #     resp.body = json.dumps(output)
     
-    def on_put(self, req, resp):
-        resp.status = falcon.HTTP_200
-        output = {
-            'msg': 'put is not supported for now - sorry :('
-        }
+    # def on_put(self, req, resp):
+    #     resp.status = falcon.HTTP_200
+    #     output = {
+    #         'msg': 'put is not supported for now - sorry :('
+    #     }
 
-        resp.body = json.dumps(output)
+    #     resp.body = json.dumps(output)
 
-    def on_delete(self, req, resp):
-        resp.status = falcon.HTTP_200
-        output = {
-            'msg': 'delete is not supported for now - sorry :('
-        }
+    # def on_delete(self, req, resp):
+    #     resp.status = falcon.HTTP_200
+    #     output = {
+    #         'msg': 'delete is not supported for now - sorry :('
+    #     }
 
-        resp.body = json.dumps(output)
+    #     resp.body = json.dumps(output)
 
 app = application = falcon.App()
 repo = ObjRequestClass()
-app.add_route('/demo', repo ) 
+app.add_route('/account', repo ) 
