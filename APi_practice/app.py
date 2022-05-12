ca4c343c4a990458a0aa3c9f62d9fa643abf3748
Dugeri James <dugeri.james@gmail.com>
2022-05-12 14:43:38 +0100

    new

--- APi_practice/app.py
+++ APi_practice/app.py
@@ -1,10 +1,10 @@
 import requests
 
-url = 'https://api.github.com/invalmid'
+url = 'https://api.github.com/'
 
 response = requests.get(url)
 
-if response.status_code == 200:
+if response:
     print('Success!')
-elif response.status_code == 404:
-    print('Not found.')
+else:
+    print('Not found. An Error occured')
