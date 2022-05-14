b0bc385ff8df643961fe464d578696b793042ffd
Dugeri James <dugeri.james@gmail.com>
2022-05-14 13:01:40 +0100

    new

---
+++ WebScraping/intro_selenium.py
@@ -0,0 +1,11 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+website = 'https://www.adamchoi.co.uk/overs/detailed'
+options = Options()
+driver = webdriver.Chrome(options=options)
+driver.get(website)
+
+driver.quit()
