e69dad3b96c1f82c2b701885388a557607614174
Dugeri James <dugeri.james@gmail.com>
2022-05-14 11:53:32 +0100

    initilazed driver

---
+++ WebScraping/post/selenium_intro.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+website = 'https://www.adamchoi.co.uk/overs/detailed'
+path = '/Users/dugerij/Downloads/chromedriver'
+driver = webdriver.Chrome(path)
+driver.get(website)
